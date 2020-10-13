7593f419c2d01bff07215588a00dcca503719984
Jessena Quinones <JQuinones13@my.gcu.edu>
2020-10-13 01:36:03 +0000

    Commit: Some of this done in class

--- InClassAssignment5/Form1.Designer.cs
+++ InClassAssignment5/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InClassAssignment5
 {
-    partial class Form1
+    partial class inClassAssignment5
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblEnterTerm = new System.Windows.Forms.Label();
+            this.txtEnterTerm = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblValueAfterPi = new System.Windows.Forms.Label();
+            this.lblCalculation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterTerm
+            // 
+            this.lblEnterTerm.AutoSize = true;
+            this.lblEnterTerm.Location = new System.Drawing.Point(128, 60);
+            this.lblEnterTerm.Name = "lblEnterTerm";
+            this.lblEnterTerm.Size = new System.Drawing.Size(81, 13);
+            this.lblEnterTerm.TabIndex = 0;
+            this.lblEnterTerm.Text = "Enter # of Term";
+            // 
+            // txtEnterTerm
+            // 
+            this.txtEnterTerm.Location = new System.Drawing.Point(215, 57);
+            this.txtEnterTerm.Name = "txtEnterTerm";
+            this.txtEnterTerm.Size = new System.Drawing.Size(100, 20);
+            this.txtEnterTerm.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(215, 113);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblValueAfterPi
+            // 
+            this.lblValueAfterPi.AutoSize = true;
+            this.lblValueAfterPi.Location = new System.Drawing.Point(128, 163);
+            this.lblValueAfterPi.Name = "lblValueAfterPi";
+            this.lblValueAfterPi.Size = new System.Drawing.Size(35, 13);
+            this.lblValueAfterPi.TabIndex = 3;
+            this.lblValueAfterPi.Text = "label1";
+            // 
+            // lblCalculation
+            // 
+            this.lblCalculation.AutoSize = true;
+            this.lblCalculation.Location = new System.Drawing.Point(128, 199);
+            this.lblCalculation.Name = "lblCalculation";
+            this.lblCalculation.Size = new System.Drawing.Size(35, 13);
+            this.lblCalculation.TabIndex = 4;
+            this.lblCalculation.Text = "label2";
+            // 
+            // inClassAssignment5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblCalculation);
+            this.Controls.Add(this.lblValueAfterPi);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtEnterTerm);
+            this.Controls.Add(this.lblEnterTerm);
+            this.Name = "inClassAssignment5";
+            this.Text = "Approximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEnterTerm;
+        private System.Windows.Forms.TextBox txtEnterTerm;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblValueAfterPi;
+        private System.Windows.Forms.Label lblCalculation;
     }
 }
 
--- InClassAssignment5/Form1.cs
+++ InClassAssignment5/Form1.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace InClassAssignment5
 {
-    public partial class Form1 : Form
+    public partial class inClassAssignment5 : Form
     {
-        public Form1()
+        public inClassAssignment5()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button to calculate pi
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //==================================
+            //        Declare variable
+            //==================================
+
+            int numLoops;
+            double valueOfPi = 0;
+            double demCount = 1;
+            bool odd = false;
+            bool even = false;
+
+            //==================================
+            //  Get # of Loops from text box
+            //==================================
+            numLoops = Convert.ToInt32(txtEnterTerm.Text);
+            //Insert try/catch
+
+            //==================================
+            //        Create for Loop
+            //==================================
+            for (int x = 1; x <= numLoops; x++)
+            {
+                //==================================
+                //  Test if loop is odd or even 
+                //==================================
+                if (x % 2 == 0)
+                {
+                    even = true;
+                    odd = false;
+                }else
+                {
+                    odd = true;
+                    even = false; 
+                }
+
+                //Is counter odd or even
+                if (odd)
+                {
+                    valueOfPi += 4 / demCount; 
+                }else
+                {
+                    valueOfPi -= 4 / demCount; 
+                }
+
+                demCount += 2; 
+            }
         }
     }
 }
